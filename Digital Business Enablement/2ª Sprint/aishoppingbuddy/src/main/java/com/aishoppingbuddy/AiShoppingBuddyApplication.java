e4eb0519195d308fe79e10f4eb7a58fcce05166d
Henry Kinoshita <42702870+PEROLOKO@users.noreply.github.com>
2023-05-03 11:23:20 -0300

    aaaaaaaaaaaaaa

---
+++ Digital Business Enablement/2ª Sprint/aishoppingbuddy/src/main/java/com/aishoppingbuddy/AiShoppingBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.aishoppingbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiShoppingBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiShoppingBuddyApplication.class, args);
+	}
+
+}
