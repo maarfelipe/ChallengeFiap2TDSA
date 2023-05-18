bdfba465f07afcb97a3afa217eb53c703388e6f0
Luiz Felipe <103139855+LuizSdO@users.noreply.github.com>
2023-05-18 17:01:26 -0300

    crud

--- Digital Business Enablement/2ª Sprint/aishoppingbuddy/src/main/java/com/aishoppingbuddy/controller/ParceiroController.java
+++ Digital Business Enablement/2ª Sprint/aishoppingbuddy/src/main/java/com/aishoppingbuddy/controller/ParceiroController.java
@@ -1,10 +1,16 @@
 package com.aishoppingbuddy.controller;
 
+import com.aishoppingbuddy.model.Parceiro;
 import com.aishoppingbuddy.repository.ParceiroRepository;
+import jakarta.validation.Valid;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("aishoppingbuddy/api/parceiro")
@@ -13,6 +19,34 @@ public class ParceiroController {
     @Autowired
     ParceiroRepository parceiroRepository;
 
-    @GetMapping()
+    @GetMapping("aishoppingbuddy/api/parceiro")
+    public List<Parceiro> getParceiros(Long id){
+        return parceiroRepository.findAll();
+    }
+    @PostMapping("aishoppingbuddy/api/parceiro")
+    public ResponseEntity<Parceiro> create(@RequestBody @Valid Parceiro parceiro, Logger log){
+        log.info("cadastrando parceiro " + parceiro);
+        parceiroRepository.save(parceiro);
+        return ResponseEntity.status(HttpStatus.CREATED).body(parceiro);
+    }
 
+    @DeleteMapping("aishoppingbuddy/api/parceiro")
+    public ResponseEntity<Parceiro> destroy(@PathVariable Long id, Logger log){
+        log.info("deletando parceiro " + id);
+        parceiroRepository.delete(getParceiros(id));
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("aishoppingbuddy/api/parceiro}")
+    public ResponseEntity<Parceiro> update(@PathVariable Long id, @RequestBody @Valid Parceiro parceiro, Logger log){
+        log.info("atualizando parceiro " + id);
+        getParceiros(id);
+        parceiro.setId(id);
+        parceiroRepository.save(parceiro);
+        return ResponseEntity.ok(parceiro);
+    }
+    private Parceiro geParceiro(Long id) {
+        return parceiroRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "parceiro não encontrado"));
+    }
 }
--- Digital Business Enablement/2ª Sprint/aishoppingbuddy/src/main/java/com/aishoppingbuddy/repository/ParceiroRepository.java
+++ Digital Business Enablement/2ª Sprint/aishoppingbuddy/src/main/java/com/aishoppingbuddy/repository/ParceiroRepository.java
@@ -5,6 +5,9 @@ import com.aishoppingbuddy.model.Parceiro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ParceiroRepository extends JpaRepository<Parceiro, Long> {
+    void delete(List<Parceiro> parceiros);
 }
