cf71bffd5fc5148483c2134aecdc5576f5bf09c4
Henry Kinoshita <42702870+PEROLOKO@users.noreply.github.com>
2023-05-19 21:19:17 -0300

    finalizado a parte de mobile

--- Hybrid Mobile App Development/2aSprint/aishoppingbuddy/App.js
+++ Hybrid Mobile App Development/2aSprint/aishoppingbuddy/App.js
@@ -4,13 +4,14 @@ import { Home } from './Screens/Home';
 import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { Image, View } from 'react-native';
+import { List } from './Screens/List';
 
 const Stack = createNativeStackNavigator();
 
 const Logo = () => {
   return (
       <View style={{ flex: 1, alignItems: 'center'}}>
-          <Image style={{width:150, height:37.5, marginLeft:-50}} source={require('./Assets/logo_poggers.png')} />
+          <Image style={{width:175, height:25, marginLeft:-50}} source={require('./assets/logo_title.png')} />
       </View>
   )
 }
@@ -20,7 +21,7 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="home" component={Home} options={{ headerTitle: () =>  <Logo />}} />
-        <Stack.Screen name="list" component={Home} options={{ headerTitle: "Todas as Recomendações"}} />
+        <Stack.Screen name="list" component={List} options={{ headerTitle: 'Todas as Recomendações'}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- Hybrid Mobile App Development/2aSprint/aishoppingbuddy/Screens/Home/index.js
+++ Hybrid Mobile App Development/2aSprint/aishoppingbuddy/Screens/Home/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-const Home = () => {
+import LinearGradient from 'react-native-linear-gradient';
+
+const Home = (props) => {
+    const { navigation } = props;
 
     const style = StyleSheet.create({
         layer: {
@@ -10,82 +13,124 @@ const Home = () => {
         },
         button: {
             width:'46%',
-            height:130,
-            backgroundColor:'#F00',
+            height:140,
             margin:'2%',
             borderRadius:10,
-            padding:10,
         },
         buttonLarge: {
             width:'96%',
-            backgroundColor:'#29F',
-            padding:16,
+        },
+        touchLarge: {
             flexDirection:'row',
-            justifyContent:'space-between'
+            justifyContent:'space-between',
+            padding:16,
+        },
+        touch: {
+            flexDirection:'column',
+            justifyContent:'space-between',
+            padding:12,
         },
         image: {
-            width:60,
-            height:60,
+            width:50,
+            height:50,
+            marginBottom:5,
         },
         logo: {
-            width:100,
-            height:100,
+            width:80,
+            height:88.25,
+            marginTop:8,
+            marginRight:12,
+        },
+        shadow: {
+            width:148,
+            height:23.75,
+            position:'absolute',
+            top:115,
+            left:246,
+            borderBottomRightRadius:10,
         },
         label: {
             color:'#FFF',
             fontSize:18,
-            fontWeight:'500'
+            fontWeight:'bold',
+            fontFamily:'Poppins',
+        },
+        title: {
+            color:'#FFF',
+            fontSize:20,
+            fontWeight:'bold',
+            fontFamily:'Poppins',
         },
         desc: {
             marginTop:5,
             color:'#FFF',
             fontSize:16,
-            fontWeight:'200'
-        },
-        b1: {
-            backgroundColor:'#83F'
-        },
-        b2: {
-            backgroundColor:'#4CF'
-        },
-        b3: {
-            backgroundColor:'#444'
-        },
-        b4: {
-            backgroundColor:'#44F'
+            fontFamily:'Poppins',
         },
     });
 
     return (
         <View>
             <View style={style.layer}>
-                <TouchableOpacity style={[style.button,style.buttonLarge]}>
-                    <View>
-                        <Text style={style.label}>{'Make better\nshop carts'}</Text>
-                        <Text style={style.desc}>{'Confira as\nrecomendações enviadas'}</Text>
-                    </View>
-                    <Image style={style.logo} source={require('.../Assets/logo.png')} />
-                </TouchableOpacity>
+                <LinearGradient
+                    colors={['#881AFF','#32C7F1']}
+                    useAngle={true}
+                    angle={135}
+                    style={[style.button,style.buttonLarge]}>
+                    <TouchableOpacity style={style.touchLarge} onPress={() => navigation.push('list', {id: Math.random()})}>
+                        <View>
+                            <Text style={style.title}>{'Make better\nshop carts'}</Text>
+                            <Text style={style.desc}>{'Confira as\nrecomendações enviadas'}</Text>
+                        </View>
+                        <Image style={style.shadow} source={require('../../assets/shadow.png')} />
+                        <Image style={style.logo} source={require('../../assets/logo_white.png')} />
+                    </TouchableOpacity>
+                </LinearGradient>
             </View>
             <View style={style.layer}>
-                <TouchableOpacity style={[style.button,style.b1]}>
-                    <Image style={style.image} source={require('.../Assets/icon1.png')} />
-                    <Text style={style.label}>{'Todas as\nRecomendações'}</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={[style.button,style.b2]}>
-                    <Image style={style.image} source={require('.../Assets/icon2.png')} />
-                    <Text style={style.label}>{'Buscar por\nID'}</Text>
-                </TouchableOpacity>
+                <LinearGradient
+                    colors={['#A34FFF','#8515FF']}
+                    useAngle={true}
+                    angle={243}
+                    style={[style.button]}>
+                    <TouchableOpacity style={style.touch}>
+                        <Image style={style.image} source={require('../../assets/icon1.png')} />
+                        <Text style={style.label}>{'Todas as\nRecomendações'}</Text>
+                    </TouchableOpacity>
+                </LinearGradient>
+                <LinearGradient
+                    colors={['#63D4F4','#2FC6F1']}
+                    useAngle={true}
+                    angle={243}
+                    style={[style.button]}>
+                    <TouchableOpacity style={style.touch}>
+                        <Image style={style.image} source={require('../../assets/icon2.png')} />
+                        <Text style={style.label}>{'Buscar por\nID'}</Text>
+                    </TouchableOpacity>
+                </LinearGradient>
             </View>
             <View style={style.layer}>
-                <TouchableOpacity style={[style.button,style.b3]}>
-                    <Image style={style.image} source={require('.../Assets/icon3.png')} />
-                    <Text style={style.label}>{'Buscar por\ndata'}</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={[style.button,style.b4]}>
-                    <Image style={style.image} source={require('.../Assets/icon4.png')} />
-                    <Text style={style.label}>{'Buscar por\nusuário'}</Text>
-                </TouchableOpacity>
+                <LinearGradient
+                    colors={['#5A5757','#231F20']}
+                    useAngle={true}
+                    angle={243}
+                    style={[style.button]}>
+                    <TouchableOpacity style={style.touch}>
+                        <Image style={style.image} source={require('../../assets/icon3.png')} />
+                        <Text style={style.label}>{'Buscar por\ndata'}</Text>
+                    </TouchableOpacity>
+                </LinearGradient>
+                <LinearGradient
+                    colors={['#6369F4','#2F37F1']}
+                    useAngle={true}
+                    angle={243}
+                    style={[style.button]}>
+                    <TouchableOpacity style={style.touch}>
+                        <Image style={style.image} source={require('../../assets/icon4.png')} />
+                        <Text style={style.label}>{'Buscar por\nusuário'}</Text>
+                    </TouchableOpacity>
+                </LinearGradient>
+                
             </View>
         </View>
     )
