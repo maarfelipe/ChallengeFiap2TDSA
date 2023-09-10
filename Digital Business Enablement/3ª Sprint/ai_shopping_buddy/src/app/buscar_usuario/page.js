4dccdb15dc5cbec3355d51a6a0de563045c2de58
maarfelipe <matheus.maarfe@hotmail.com>
2023-09-10 17:35:37 -0300

    tentando conectar

--- Digital Business Enablement/3ª Sprint/ai_shopping_buddy/src/app/buscar_usuario/page.js
+++ Digital Business Enablement/3ª Sprint/ai_shopping_buddy/src/app/buscar_usuario/page.js
@@ -1,9 +1,26 @@
 import SideBar from "@/components/SideBar";
 
-export default function Buscar_Usuario() {
+async function getUsuarios() {
+	const url = "http://localhost:8080/aishoppingbuddy/api/usuario"
+	const resp = await fetch(url, {header: {Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJlbWFpbDFAZ21haWwuY29tIiwiaXNzIjoiQUlTaG9wcGluZ0J1ZGR5IiwiZXhwIjoxNjk0MzkxMDQ5fQ.2gtRBTTslk7vdMYsWtN41VY1r6HZ-XRwTc9Icr2hlyo"} })
+	const data = await resp.json()
+	console.log(data) 
+}
+
+export default async function BuscarUsuario() {
+
 	return (
-		<>
-			<SideBar active={"buscar_usuario"}/>
-		</>
+		<div className="flex">
+			{/* <div className="w-1/5">
+				<SideBar active={"buscar_usuario"} />
+			</div> */}
+
+			<div className="w-4/5 p-4">
+				<h1 className="text-2xl font-bold mb-4">Welcome to the AI Shopping Buddy</h1>
+				<p>
+					teste
+				</p>
+			</div>
+		</div>
 	)
 }
--- Digital Business Enablement/3ª Sprint/ai_shopping_buddy/src/app/page.js
+++ Digital Business Enablement/3ª Sprint/ai_shopping_buddy/src/app/page.js
@@ -1,9 +1,20 @@
 import SideBar from "@/components/SideBar";
+import React from "react";
+
 
 export default function Home() {
 	return (
-		<>
-			<SideBar />
-		</>
+		<div className="flex">
+			<div className="w-1/5">
+				<SideBar />
+			</div>
+
+			<div className="w-4/5 p-4">
+				<h1 className="text-2xl font-bold mb-4">Welcome to the AI Shopping Buddy</h1>
+				<p>
+					This is where the content of your main page goes.
+				</p>
+			</div>
+		</div>
 	)
 }
