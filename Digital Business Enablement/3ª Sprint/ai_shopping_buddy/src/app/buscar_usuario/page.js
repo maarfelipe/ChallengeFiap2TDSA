3b5c054abeb906bfd517ea4aafc14f1805aab0ef
maarfelipe <matheus.maarfe@hotmail.com>
2023-09-10 18:01:20 -0300

    Update page.js

--- Digital Business Enablement/3ª Sprint/ai_shopping_buddy/src/app/buscar_usuario/page.js
+++ Digital Business Enablement/3ª Sprint/ai_shopping_buddy/src/app/buscar_usuario/page.js
@@ -2,11 +2,17 @@ import SideBar from "@/components/SideBar";
 
 async function getUsuarios() {
 	const url = "http://localhost:8080/aishoppingbuddy/api/usuario"
+<<<<<<< Updated upstream
 	const resp = await fetch(
 		url,
 		{headers:{Authorization:'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJlbWFpbDFAZ21haWwuY29tIiwiaXNzIjoiQUlTaG9wcGluZ0J1ZGR5IiwiZXhwIjoxNjk0MzkzNDU1fQ.k1fZOy305rNnhbo5F3NHVx1fXFaVplnHYtjHW4tfPNY'}}
 	)
 	return resp.json();
+=======
+	const resp = await fetch(url, { header: { Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJlbWFpbDFAZ21haWwuY29tIiwiaXNzIjoiQUlTaG9wcGluZ0J1ZGR5IiwiZXhwIjoxNjk0MzkxMDQ5fQ.2gtRBTTslk7vdMYsWtN41VY1r6HZ-XRwTc9Icr2hlyo" } })
+	const data = await resp.json()
+	console.log(data)
+>>>>>>> Stashed changes
 }
 
 export default async function BuscarUsuario() {
@@ -14,6 +20,7 @@ export default async function BuscarUsuario() {
 	const data = await getUsuarios();
 
 	return (
+<<<<<<< Updated upstream
 		<div className="flex">
 			<div className="w-1/5">
 				<SideBar active={"buscar_usuario"} />
@@ -24,7 +31,17 @@ export default async function BuscarUsuario() {
 				<p>
 					{JSON.stringify(data)}
 				</p>
+=======
+
+		<div className="flex">
+			<div className="w-64">
+				<SideBar active={"buscar_usuario"} />
+			</div>
+			<div className="flex-grow">
+				Buscar Usuários
+>>>>>>> Stashed changes
 			</div>
 		</div>
+
 	)
 }
