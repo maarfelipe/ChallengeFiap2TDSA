aa535af2e3ea836d98c035a59ae428bf3a003773
maarfelipe <matheus.maarfe@hotmail.com>
2023-09-10 18:01:08 -0300

    Update page.js

--- Digital Business Enablement/3ª Sprint/ai_shopping_buddy/src/app/page.js
+++ Digital Business Enablement/3ª Sprint/ai_shopping_buddy/src/app/page.js
@@ -1,20 +1,15 @@
-import SideBar from "@/components/SideBar";
 import React from "react";
+import SideBar from "@/components/SideBar";
 
-
-export default function Home() {
-	return (
-		<div className="flex">
-			<div className="w-1/5">
-				<SideBar />
-			</div>
-
-			<div className="w-4/5 p-4">
-				<h1 className="text-2xl font-bold mb-4">Welcome to the AI Shopping Buddy</h1>
-				<p>
-					This is where the content of your main page goes.
-				</p>
-			</div>
-		</div>
-	)
+export default function HomePage() {
+  return (
+    <div className="flex">
+      <div className="w-64">
+        <SideBar />
+      </div>
+      <div className="flex-grow">
+        Home
+      </div>
+    </div>
+  );
 }
