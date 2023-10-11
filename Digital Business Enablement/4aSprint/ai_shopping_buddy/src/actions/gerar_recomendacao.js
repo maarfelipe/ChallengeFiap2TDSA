2e3009a587bf5b779d36374d8e8892c3aa1eaa51
EnzoPerazolo <perazoloenzo@gmail.com>
2023-10-11 09:10:38 -0300

    feat: request usando token com login

--- Digital Business Enablement/4aSprint/ai_shopping_buddy/src/actions/gerar_recomendacao.js
+++ Digital Business Enablement/4aSprint/ai_shopping_buddy/src/actions/gerar_recomendacao.js
@@ -1,16 +1,16 @@
 "use server"
 
 import { revalidatePath } from "next/cache";
-import { cookies } from 'next/headers'
+import { cookies } from "next/headers";
 
-export async function create(formData) {
-    const url = "http://localhost:8080/aishoppingbuddy/api/usuario";
+export async function create(formData){
+    const url = "http://localhost:8080/aishoppingbuddy/api/recomendacao";
     const token = cookies().get("aishoppingbuddy_token")
     console.log(token);
-    console.log({ formData });
+    console.log(JSON.stringify(formData));
     const options = {
         method: "POST",
-        body: formData ? JSON.stringify(formData) : null,
+        body: JSON.stringify(formData),
         headers: {
             "Content-Type": "application/json",
             "Authorization": `Bearer ${token.value}`
@@ -19,11 +19,11 @@ export async function create(formData) {
     try {
         console.log({options})
         const resp = await fetch(url, options);
-        console.log({resp})
-        revalidatePath("/buscar_usuario")
+        console.log({resp});
+        revalidatePath("/recomendacao")
         return { message: "ok" }
     } catch (error) {
         console.log({error})
-        return { message: "Erro ao cadastrar" }
+        return { message: "Erro ao gerar" }
     }
 }
--- Digital Business Enablement/4aSprint/ai_shopping_buddy/src/app/gerar_recomendacao/page.js
+++ Digital Business Enablement/4aSprint/ai_shopping_buddy/src/app/gerar_recomendacao/page.js
@@ -1,6 +1,25 @@
 import SideBar from "@/components/SideBar";
+import { useState } from "react";
+import { create } from "@/actions/cadastrar_usuario";
 
 export default function Gerar_Recomendacao() {
+	const [erro, setErro] = useState("");
+	const router = useStateseRouter();
+
+	const [ listProduto, setListProduto ] = useState("") ;
+
+	async function onCreate() {
+		const formData = {
+			listProduto: listProduto,
+		}
+		const resp = await create(formData);
+		if (resp.message === "ok") {
+			router.push("/ver_recomendacoes");
+			return;
+		}
+		setErro(resp.message);
+	}
+
 	return (
 		<>
 			<SideBar active={"gerar_recomendacao"}/>
